5e88862772d8e9eaa1f34ce5bc61e398e214f628
Luca Lambiase <luca.lambia@gmail.com>
2023-06-27 14:06:31 +0200

    public assets workaround

--- config/store.php
+++ config/store.php
@@ -2,7 +2,10 @@
 
 return [
     'chiave' => "Ciao mondo",
+    //Logo caricato da URL online
     'logo' => "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1969px-Laravel.svg.png",
+    //Logo caricato dalla cartella public/img/logo.png, per chi avesse problemi con Vite
+    // 'logo' => "img/logo.png",
     'someLinks' => [
         [
             "href" => "https://laravel.com/docs/9.x",
